a39e45d92b979afb1e7f71b107e41d75e7835537
Yi Zheng Tang <G00425075@atu.ie>
2023-10-16 17:33:11 +0100

    Added division feature into the calculator.

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
     public static void main(String[] args) {
         add();
         multiply();
+        divide();
     }
 
     public static void add() {
@@ -34,6 +35,20 @@ public class Calculator {
 
         System.out.println("The multiply of two numbers is: " + total);
 
+    }
+
+    public static void divide()
+    {
+        System.out.println("Please enter your first number: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter your second number: ");
+        int secondNumber = inputs.nextInt();
+
+        int ans = firstNumber / secondNumber;
+
+        System.out.println("The division of the two numbers is: " + ans);
     }
 
 }
